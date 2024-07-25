fd93a630f4faca08dca46a58d09d4f048deaa811
kookie270202 <120020356+kookie270202@users.noreply.github.com>
2024-07-25 20:40:49 +0530

    commit message

---
+++ views/intersectionobserver.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    const lazyImages = document.querySelectorAll('.lazy');
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    lazyImages.forEach(image => {
+        imageObserver.observe(image);
+    });
+
+
+})
